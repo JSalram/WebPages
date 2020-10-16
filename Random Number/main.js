e226e0dfdd439be2c3e82fe8d374a00d90e4af2d
Javi <javisr997@gmail.com>
2020-10-16 13:47:11 +0200

    Mejoras Randomize

--- Random Number/main.js
+++ Random Number/main.js
@@ -1,29 +1,40 @@
-var nums = []
+var nums = [];
+var max = 23;
+var aciertos = 0;
 
-var randNum1 = Math.round(Math.random() * 30);
-var randNum2 = Math.round(Math.random() * 30);
+var randNum1 = Math.round(Math.random() * max);
+var randNum2 = Math.round(Math.random() * max);
 while (randNum1 == randNum2)
 {
-    randNum2 = Math.round(Math.random() * 30);
+    randNum2 = Math.round(Math.random() * max);
 }
 
 function random(i)
 {
+    var restantes = document.getElementById("restantes");
     var botones = document.getElementsByTagName("button");
     var boton = botones[i];
 
-    if (boton.innerHTML-1 == randNum1 || boton.innerHTML-1 == randNum2)
+    if (aciertos < 2)
     {
-        boton.style.backgroundColor = "black";
+        if (boton.innerHTML-1 == randNum1 || boton.innerHTML-1 == randNum2)
+        {
+            boton.style.backgroundColor = "black";
+            aciertos += 1;
+        }
+        else
+        {
+            boton.style.backgroundColor = "cadetblue";
+        }
+        
+        boton.style.color = "white";    
+        nums.splice(nums.indexOf(i), 1);
+        restantes.innerHTML = "Intentos restantes: " + nums.length;
     }
-    else
+    if (aciertos == 2)
     {
-        boton.style.backgroundColor = "cadetblue";
+        restantes.innerHTML = "FINISHED!"
     }
-    
-    boton.style.color = "white";
-    
-    nums.splice(nums.indexOf(i), 1);
 }
 
 function info()
@@ -44,7 +55,7 @@ window.onload = function()
     var botonera = document.getElementById("botonera");
     var botones = ""
         
-    for (var i = 0; i < 30; i++)
+    for (var i = 0; i <= max; i++)
     {
         botones += "<button onclick='random(" + i + ")'>" + (i+1) + "</button>"
         nums[nums.length] = i;
